9c986f9af6fd99aae51e1d76dd9dadf24cd430c0
David Birks <davidebirks@gmail.com>
2016-09-22 23:57:39 -0400

    input and output disk selectors should be functional now

--- main.py
+++ main.py
@@ -27,29 +27,49 @@ class InitialScreen(GridLayout):
             out = out.rstrip(' \n')             
             disk_models[i] = out
 
-        #def choose_disk(instance):
-        #    cmd = 'sudo hdparm -I /dev/sda | grep "Model" | sed "s/[[:space:]]Model Number:[[:space:]]\+//g"'
-        #    call(str(cmd), shell=True)
-            
-        self.cols = 2
-
-        def set_input(disk):
-            input_disk = disk_names[disk]
-            print(input_disk)
+        self.cols = 3
+        self.rows = 2
+        self.spacing = [5,40]
+        input_disk = 'bleh'
+        output_disk = 'meh'
 
         # column 1
         self.add_widget(Label(text='Input disk:'))
-        for i, disk in enumerate(disk_names):
-            btn = Button(text=disk_names[i] + '\n' + disk_models[i])
-            self.add_widget(btn)
-            btn.bind(on_press = set_input(str(i))) ######
-        
-        
+        # button 1
+        def set_input(instance):
+            global input_disk 
+            input_disk = disk_names[0]
+        btn1 = Button(text=disk_names[0] + '\n' + disk_models[0])
+        self.add_widget(btn1)
+        btn1.bind(on_press = set_input)
+        # button 2
+        def set_input(instance):
+            global input_disk 
+            input_disk = disk_names[1]
+        btn2= Button(text=disk_names[1] + '\n' + disk_models[1])
+        self.add_widget(btn2)
+        btn2.bind(on_press = set_input)
+
         # column 2
         self.add_widget(Label(text='Output disk:'))
-        btnOutput1 = Button(text='sda')
-        self.add_widget(btnOutput1)
-        self.add_widget(Button(text='sdb'))
+        # button 3
+        def set_output(instance):
+            global output_disk 
+            output_disk = disk_names[0]
+        btn3 = Button(text=disk_names[0] + '\n' + disk_models[0])
+        self.add_widget(btn3)
+        btn3.bind(on_press = set_output)
+        # button 4
+        def set_output(instance):
+            global output_disk 
+            output_disk = disk_names[1]
+        btn4= Button(text=disk_names[1] + '\n' + disk_models[1])
+        self.add_widget(btn4)
+        btn4.bind(on_press = set_output)
+
+        print('in:' + input_disk)
+        print('out:' + output_disk)
+
 
 class MyApp(App):
 
